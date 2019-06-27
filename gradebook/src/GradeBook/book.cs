a9cff4b88376cbd68fa34134a364c6f7df54bb95
Patrick.Yue <patrick.yue@blucora.com>
2019-06-27 15:47:04 -0500

    encapsulation / put logic in class, not in main

--- gradebook/src/GradeBook/book.cs
+++ gradebook/src/GradeBook/book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -5,9 +6,10 @@ namespace GradeBook
   class Book
   {
     //constractor
-    public Book()
+    public Book(string name)
     {
       grades = new List<double>();
+      this.name = name;
     }
 
     public void AddGrade(double grade)
@@ -15,7 +17,25 @@ namespace GradeBook
       grades.Add(grade);
     }
 
+    public void ShowStatiscs()
+    {
+      var result = 0.0;
+      var highGrade = double.MinValue;
+      var lowGrade = double.MaxValue;
+      foreach (var number in grades)
+      {
+        highGrade = Math.Max(number, highGrade);
+        lowGrade = Math.Min(number, lowGrade);
+        result += number;
+      }
+      result /= grades.Count;
+      Console.WriteLine($"The highGrade grade is {highGrade:N1}");
+      Console.WriteLine($"The lowGrade grade is {lowGrade:N1}");
+      Console.WriteLine($"The average grade is {result:N1}");
+    }
+
     // fields
-    List<double> grades;
+    private List<double> grades;
+    private string name;
   }
 }
