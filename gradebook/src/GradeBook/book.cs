581108f35ecc28847d55b0704f47e78896b4c58a
Patrick.Yue <patrick.yue@blucora.com>
2019-07-01 15:44:08 -0500

    Reference and Value, string / Class is ref

--- gradebook/src/GradeBook/book.cs
+++ gradebook/src/GradeBook/book.cs
@@ -1,27 +1,33 @@
 using System;
 using System.Collections.Generic;
 
-namespace GradeBook {
-  public class Book {
+namespace GradeBook
+{
+  public class Book
+  {
     //constractor
-    public Book (string name) {
-      grades = new List<double> ();
-      this.name = name;
+    public Book(string name)
+    {
+      grades = new List<double>();
+      Name = name;
     }
 
-    public void AddGrade (double grade) {
-      grades.Add (grade);
+    public void AddGrade(double grade)
+    {
+      grades.Add(grade);
     }
 
-    public Statistics GetStatistics () {
-      var result = new Statistics ();
+    public Statistics GetStatistics()
+    {
+      var result = new Statistics();
       result.Average = 0.0;
 
       result.High = double.MinValue;
       result.Low = double.MaxValue;
-      foreach (var grade in grades) {
-        result.High = Math.Max (grade, result.High);
-        result.Low = Math.Min (grade, result.Low);
+      foreach (var grade in grades)
+      {
+        result.High = Math.Max(grade, result.High);
+        result.Low = Math.Min(grade, result.Low);
         result.Average += grade;
       }
       result.Average /= grades.Count;
@@ -30,6 +36,6 @@ namespace GradeBook {
 
     // fields
     private List<double> grades;
-    private string name;
+    public string Name;
   }
 }
